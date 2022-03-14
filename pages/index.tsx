6f667145c07e82ded537b4feb3b70b6f6935c974
zhangweiwei <zhangzhang_10@hotmail.com>
2022-03-14 18:56:15 +0800

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,19 @@ const posts = [
 ]
 const Home: NextPage = () => {
   return (
-    <div className="container mx-auto mb-8 px-10">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CMS Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
-        <div className="lg:col-spa-8 col-span-1">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
             <PostCard post={post} key={post.title} />
           ))}
         </div>
-        <div className="col-span-1 lg:col-span-4 ">
-          <div className="relative top-8 lg:sticky ">
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
             <PostWidget />
             <Categories />
           </div>
