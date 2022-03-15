ab8c3399f92f5e81cd7a639e72a49bccb1874a90
zhangweiwei <zhangzhang_10@hotmail.com>
2022-03-15 18:13:42 +0800

    change

---
+++ pages/index.jsx
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import { PostCard, PostWidget, Categories } from '../components'
+import { getPosts } from '../services'
+
+function Home({ posts }) {
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <Head>
+        <title>CMS Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          {posts.map((post) => (
+            <PostCard post={post.node} key={post.title} />
+          ))}
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || []
+  return {
+    props: {
+      posts,
+    },
+  }
+}
+
+export default Home
