a714f9f56433826c7afea73dc07b6f78be4a27a4
zhangweiwei <zhangzhang_10@hotmail.com>
2022-03-20 18:34:41 +0800

    change

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -46,6 +46,6 @@ export async function getStaticPaths() {
   const categories = await getCategories()
   return {
     paths: categories.map(({ slug }) => ({ params: { slug } })),
-    fallback: true,
+    fallback: false,
   }
 }
